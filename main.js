a5a6d50461dc981364f5428ce7630b678a54cbba
Mikołaj Muller <mikolaj.muller23@gmail.com>
2014-07-02 22:02:43 +0200

    added welcome using your nick

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
+var myArgs = process.argv;
 
-(function() {
-	console.log("hello!!!");
-}).call(this)
+var name = myArgs[2];
+
+if(name) {
+  console.log("Hello " + name + "!!!");
+}else {
+  console.log("Hello!!!!");
+  console.log("");
+  console.log("Now try agan: xxheloname MY_NAME");
+}
+
+//(function() {
+//	
+//	console.log("hello!!!");
+//}).call(this)
 
